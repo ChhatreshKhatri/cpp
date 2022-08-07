69d27c53341038ca38c43f335e25660186d4fe41
chhatreshkhatri <chhatresh.khatri1002@gmail.com>
2022-08-07 19:09:20 +0530

    level order

--- DSA/036_print_level_order_traversal.cpp
+++ DSA/036_print_level_order_traversal.cpp
@@ -51,8 +51,64 @@ void printLevelWise(BinaryTreeNode<int> *root)
         }
     }
 }
+//recursion
+void printLevel(BinaryTreeNode<int> *node,int level){
+    if(node==NULL)return;
+    if(level==0)cout<<node->data<<" ";
+    else if(level>0){
+        printLevel(node->left,level-1);
+        printLevel(node->right,level-1);
+    }
+}
+//using queue
+void printLevelOrder(BinaryTreeNode<int>* root)
+{
+    // Base Case
+    if (root == NULL)
+        return;
+ 
+    // Create an empty queue for level order traversal
+    queue<BinaryTreeNode<int>*> q;
+ 
+    // Enqueue Root and initialize height
+    q.push(root);
+ 
+    while (q.empty() == false) {
+        // Print front of queue and remove it from queue
+        BinaryTreeNode<int>* node = q.front();
+        cout << node->data << " ";
+        q.pop();
+ 
+        /* Enqueue left child */
+        if (node->left != NULL)
+            q.push(node->left);
+ 
+        /*Enqueue right child */
+        if (node->right != NULL)
+            q.push(node->right);
+    }
+}
+
 int main()
 {
+    BinaryTreeNode<int> *node=new BinaryTreeNode(10);
+    node->left=new BinaryTreeNode(20);
+    node->right=new BinaryTreeNode(30);
+    node->left->left=new BinaryTreeNode(40);
+    node->left->right=new BinaryTreeNode(50);
+    node->left->left->left=new BinaryTreeNode(60);
 
+    cout<<"with height"<<endl;
+    //with height 
+    // TC-> O(n^2) SC->O(n) n is nbo. of nodes in tree
+    //3 is height of tree
+    for(int i=0;i<=3;i++)printLevel(node,i);
+    cout<<endl<<endl;
+
+    //wiithout height
+    printLevelOrder(node);
+    cout<<endl<<endl;
+    cout<<"without height"<<endl;
+    printLevelWise(node);
     return 0;
 }
