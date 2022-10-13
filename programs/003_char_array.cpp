eb12705d21cae87b4286c963f09134e251535317
chhatresh <chhatresh.khatri1002@gmail.com>
2022-10-13 10:46:24 +0530

    array

---
+++ programs/003_char_array.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    char str[]={'g','f','g'};
+    cout<<str<<endl;
+    char str2[]={'g','f','g','\0'};
+    cout<<str2;
+
+    return 0;
+}
